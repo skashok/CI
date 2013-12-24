0d1c223a14df577ad1ae7b266a237eb0fc13fe12
Ed Elliott <edelliott@nc.rr.com>
2013-12-24 09:19:18 -0500

    Latest try at Android Unit Tests

---
+++ GradleTest/app/src/instrumentTest/java/FirstTest.java
@@ -0,0 +1,11 @@
+package net.servicetrackingsystems.gradletest.test;
+
+import android.test.ActivityTestCase;
+
+package
+/**
+ * Created by Ed on 12/20/13.
+ */
+public class FirstTest extends ActivityTestCase
+{
+}
